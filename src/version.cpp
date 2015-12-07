b7e3966a11852dd51c3a78ac9fb4a81e1d6f44f1
Spidersbox <Spider@Spidersbox.com>
2015-12-06 19:10:39 -0800

    Updated version for Bitbar-qt

--- src/version.cpp
+++ src/version.cpp
@@ -37,7 +37,8 @@ const std::string CLIENT_NAME("BitBar");
 #define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE
 #    define GIT_COMMIT_ID "32a928e"
-#    define GIT_COMMIT_DATE "$Format:%cD"
+//#    define GIT_COMMIT_DATE "$Format:%cD"
+#    define GIT_COMMIT_DATE "Dec 6 2015"
 #endif
 
 #define BUILD_DESC_FROM_COMMIT(maj,min,rev,build,commit) \
